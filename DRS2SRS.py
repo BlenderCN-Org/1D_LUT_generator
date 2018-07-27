2b75bef497d75fecfa164f2e3295c7ec37fb73d8
ndopu <ndopumaboshe@gmail.com>
2018-07-28 01:06:34 +0200

    Added function to calculate geometric mean and function to normalize the exposure and RGB values.

--- DRS2SRS.py
+++ DRS2SRS.py
@@ -1,7 +1,41 @@
 import pandas as pd
 import numpy as np
 
-file = "drs2srs.xls"
-xl = pd.ExcelFile(file)
-df = xl.parse("Sheet1")
-df.head()
+def geometric_mean(nums):
+    '''
+    Computes the geometric mean of nums.
+    Given a list nums = x1,x2...xn, the mean is given as:
+
+            mean = (x1*x2...*xn)**(1/n)
+
+    :param nums:
+    :return geometric mean of nums:
+    '''
+    return [(reduce(lambda x, y: x * y, l)) ** (1.0 / len(l)) for l in nums]
+
+
+def normalize(data):
+    '''
+    Normalizes exposure and rgb values between 0 and 1.
+
+    :param nums:
+    :return normalized exposure and rgb values:
+    '''
+
+    # Normalizing exposure
+    min_val = data['Exposure'].min()
+    max_val = data['Exposure'].max()
+    val_range = max_val - min_val
+    norm_exposure = (data['Exposure'] - min_val) / (val_range)
+
+    # Normalizing RGB values
+    norm_rgb = data[['R','G','B']] / 255.0
+
+    return  pd.concat([norm_exposure, norm_rgb], axis=1)
+
+csv_file = "drs2srs.csv"
+df = pd.read_csv(csv_file)
+
+d = normalize(df)
+gmean = geometric_mean(d[['R','G','B']].values.tolist())
+print(gmean)
